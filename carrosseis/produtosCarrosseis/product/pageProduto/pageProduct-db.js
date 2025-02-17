c63c139a9d3b4ec5f06af80d0746f62d0d047607
Walter Miranda <101981943+powalter@users.noreply.github.com>
2025-02-17 17:52:26 -0300

    Add files via upload

---
+++ carrosseis/produtosCarrosseis/product/pageProduto/pageProduct-db.js
@@ -0,0 +1,391 @@
+const products = [
+  {
+    id: 1,
+    name: 'PC Gamer Pichau MSI Monster Hunter, Intel i7-14700K, GeForce RTX 4060 Ti 8GB, 32GB DDR5, SSD M.2 1TB',
+    code: '99999',
+    price: 14999.90,
+    discount: 5,
+    description: 'PC-GAMER',
+    images: [
+      'https://assets.onecompiler.app/42wu6ajnx/438dhea88/pc_1_img_1.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/438dhea88/pc_1_img_2.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/438dhea88/pc_1_img_3.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/438dhea88/pc_1_img_4.jpeg'
+    ],
+    detailedDescription:`
+      <div class="boxDescription_content">
+        <p><strong>Projetado para garantir o melhor desempenho em seus Jogos!</strong></p>
+  
+        <p>Os Computadores Brazil Pc, equipados com processadores Intel Core i5, têm desempenho surpreendente. Execute múltiplas tarefas ao mesmo tempo com fluidez.</p>
+        
+        <p>Compatível com os mais diversos jogos atuais, execute em alto desempenho sem lags ou travamentos, com armazenamento M.2, ligue e abra seus programas em segundos. Transfira seus dados facilmente entre dispositivos através da porta USB. Acompanha manual de instrução.</p>
+        
+        <p><strong>Especificações do produto abaixo:</strong></p>
+        
+        <p><strong>- Processador:</strong></p>
+        <p>Intel Core i5-11400 11ª geração</p>
+        
+        <p><strong>- Memória:</strong></p>
+        <p>Capacidade 16GB DDR4</p>
+        
+        <p><strong>- Armazenamento:</strong></p>
+        <p>Tipo: M.2, Capacidade: 512GB</p>
+        
+        <p><strong>- Placa de Vídeo:</strong></p>
+        <p>GEFORCE RTX3060M Golden Memory</p>
+        
+        <p><strong>- Fonte:</strong></p>
+        <p>500W Bivolt automática 80 Plus Bronze</p>
+        
+        <p><strong>- Tensão:</strong></p>
+        <p>110V/230V</p>
+        
+        <p><strong>- Gabinete:</strong></p>
+        <p>Tipo: Torre ATX com lateral em vidro temperado</p>
+      </div>
+    `
+  },
+  {
+    id: 2,
+    name: 'PC Gamer Completo Mancer Careca II, AMD Ryzen 5 5600X, GeForce RTX 2060 Super 8GB, 32GB DDR4, SSD M.2 1TB',
+    code: '99999',
+    price: 4599.90,
+    discount: 5,
+    description: 'PC-GAMER',
+    images: [
+      'https://assets.onecompiler.app/42wu6ajnx/438dhea88/pc_2_img_1.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/438dhea88/pc_2_img_2.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/438dhea88/pc_2_img_3.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/438dhea88/pc_2_img_4.jpeg'
+    ],
+    detailedDescription:`
+      <div class="boxDescription_content">
+        <p><strong>Projetado para garantir o melhor desempenho em seus Jogos!</strong></p>
+  
+        <p>Os Computadores Brazil Pc, equipados com processadores Intel Core i5, têm desempenho surpreendente. Execute múltiplas tarefas ao mesmo tempo com fluidez.</p>
+        
+        <p>Compatível com os mais diversos jogos atuais, execute em alto desempenho sem lags ou travamentos, com armazenamento M.2, ligue e abra seus programas em segundos. Transfira seus dados facilmente entre dispositivos através da porta USB. Acompanha manual de instrução.</p>
+        
+        <p><strong>Especificações do produto abaixo:</strong></p>
+        
+        <p><strong>- Processador:</strong></p>
+        <p>Intel Core i5-11400 11ª geração</p>
+        
+        <p><strong>- Memória:</strong></p>
+        <p>Capacidade 16GB DDR4</p>
+        
+        <p><strong>- Armazenamento:</strong></p>
+        <p>Tipo: M.2, Capacidade: 512GB</p>
+        
+        <p><strong>- Placa de Vídeo:</strong></p>
+        <p>GEFORCE RTX3060M Golden Memory</p>
+        
+        <p><strong>- Fonte:</strong></p>
+        <p>500W Bivolt automática 80 Plus Bronze</p>
+        
+        <p><strong>- Tensão:</strong></p>
+        <p>110V/230V</p>
+        
+        <p><strong>- Gabinete:</strong></p>
+        <p>Tipo: Torre ATX com lateral em vidro temperado</p>
+      </div>
+    `
+  },
+  {
+    id: 3,
+    name: 'PC Gamer Pichau Highflyer Hugin IV, AMD Ryzen 9 9900X, Radeon RX 7900 XT 20GB, 32GB DDR5, SSD M.2 2TB',
+    code: '99999',
+    price: 16299.00,
+    discount: 5,
+    description: 'PC-GAMER',
+    images: [
+      'https://assets.onecompiler.app/42wu6ajnx/438dhea88/pc_3_img_1.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/438dhea88/pc_3_img_2.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/438dhea88/pc_3_img_3.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/438dhea88/pc_3_img_4.jpeg'
+    ],
+    detailedDescription:`
+      <div class="boxDescription_content">
+        <p><strong>Projetado para garantir o melhor desempenho em seus Jogos!</strong></p>
+  
+        <p>Os Computadores Brazil Pc, equipados com processadores Intel Core i5, têm desempenho surpreendente. Execute múltiplas tarefas ao mesmo tempo com fluidez.</p>
+        
+        <p>Compatível com os mais diversos jogos atuais, execute em alto desempenho sem lags ou travamentos, com armazenamento M.2, ligue e abra seus programas em segundos. Transfira seus dados facilmente entre dispositivos através da porta USB. Acompanha manual de instrução.</p>
+        
+        <p><strong>Especificações do produto abaixo:</strong></p>
+        
+        <p><strong>- Processador:</strong></p>
+        <p>Intel Core i5-11400 11ª geração</p>
+        
+        <p><strong>- Memória:</strong></p>
+        <p>Capacidade 16GB DDR4</p>
+        
+        <p><strong>- Armazenamento:</strong></p>
+        <p>Tipo: M.2, Capacidade: 512GB</p>
+        
+        <p><strong>- Placa de Vídeo:</strong></p>
+        <p>GEFORCE RTX3060M Golden Memory</p>
+        
+        <p><strong>- Fonte:</strong></p>
+        <p>500W Bivolt automática 80 Plus Bronze</p>
+        
+        <p><strong>- Tensão:</strong></p>
+        <p>110V/230V</p>
+        
+        <p><strong>- Gabinete:</strong></p>
+        <p>Tipo: Torre ATX com lateral em vidro temperado</p>
+      </div>
+    `
+  },
+  {
+    id: 4,
+    name: 'PC Gamer Pichau Stronda, AMD Ryzen 7 5700X3D, GeForce RTX 4070 Super 12GB, 16GB DDR4, SSD M.2 1TB',
+    code: '99999',
+    price: 9890.00,
+    discount: 5,
+    description: 'PC-GAMER',
+    images: [
+      'https://assets.onecompiler.app/42wu6ajnx/438dhea88/pc_4_img_1.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/438dhea88/pc_4_img_2.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/438dhea88/pc_4_img_3.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/438dhea88/pc_1_img_4.jpeg'
+    ],
+    detailedDescription:`
+      <div class="boxDescription_content">
+        <p><strong>Projetado para garantir o melhor desempenho em seus Jogos!</strong></p>
+  
+        <p>Os Computadores Brazil Pc, equipados com processadores Intel Core i5, têm desempenho surpreendente. Execute múltiplas tarefas ao mesmo tempo com fluidez.</p>
+        
+        <p>Compatível com os mais diversos jogos atuais, execute em alto desempenho sem lags ou travamentos, com armazenamento M.2, ligue e abra seus programas em segundos. Transfira seus dados facilmente entre dispositivos através da porta USB. Acompanha manual de instrução.</p>
+        
+        <p><strong>Especificações do produto abaixo:</strong></p>
+        
+        <p><strong>- Processador:</strong></p>
+        <p>Intel Core i5-11400 11ª geração</p>
+        
+        <p><strong>- Memória:</strong></p>
+        <p>Capacidade 16GB DDR4</p>
+        
+        <p><strong>- Armazenamento:</strong></p>
+        <p>Tipo: M.2, Capacidade: 512GB</p>
+        
+        <p><strong>- Placa de Vídeo:</strong></p>
+        <p>GEFORCE RTX3060M Golden Memory</p>
+        
+        <p><strong>- Fonte:</strong></p>
+        <p>500W Bivolt automática 80 Plus Bronze</p>
+        
+        <p><strong>- Tensão:</strong></p>
+        <p>110V/230V</p>
+        
+        <p><strong>- Gabinete:</strong></p>
+        <p>Tipo: Torre ATX com lateral em vidro temperado</p>
+      </div>
+    `
+  },
+  {
+    id: 5,
+    name: 'PC Gamer Fuzhu IV, i7-12700F, RTX 4060 Ti 8GB, 32GB, SSD M.2 1TB',
+    code: '99999',
+    price: 12905.00,
+    discount: 5,
+    description: 'PC-GAMER',
+    images: [
+      'https://assets.onecompiler.app/42wu6ajnx/4392yzfbn/pc_5_img_1.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/4392yzfbn/pc_5_img_2.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/4392yzfbn/pc_5_img_3.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/4392yzfbn/pc_5_img_4.jpeg',
+    ],
+    detailedDescription:`
+      <div class="boxDescription_content">
+        <p><strong>Projetado para garantir o melhor desempenho em seus Jogos!</strong></p>
+  
+        <p>Os Computadores Brazil Pc, equipados com processadores Intel Core i5, têm desempenho surpreendente. Execute múltiplas tarefas ao mesmo tempo com fluidez.</p>
+        
+        <p>Compatível com os mais diversos jogos atuais, execute em alto desempenho sem lags ou travamentos, com armazenamento M.2, ligue e abra seus programas em segundos. Transfira seus dados facilmente entre dispositivos através da porta USB. Acompanha manual de instrução.</p>
+        
+        <p><strong>Especificações do produto abaixo:</strong></p>
+        
+        <p><strong>- Processador:</strong></p>
+        <p>Intel Core i5-11400 11ª geração</p>
+        
+        <p><strong>- Memória:</strong></p>
+        <p>Capacidade 16GB DDR4</p>
+        
+        <p><strong>- Armazenamento:</strong></p>
+        <p>Tipo: M.2, Capacidade: 512GB</p>
+        
+        <p><strong>- Placa de Vídeo:</strong></p>
+        <p>GEFORCE RTX3060M Golden Memory</p>
+        
+        <p><strong>- Fonte:</strong></p>
+        <p>500W Bivolt automática 80 Plus Bronze</p>
+        
+        <p><strong>- Tensão:</strong></p>
+        <p>110V/230V</p>
+        
+        <p><strong>- Gabinete:</strong></p>
+        <p>Tipo: Torre ATX com lateral em vidro temperado</p>
+      </div>
+    `
+  },
+  {
+    id: 6,
+    name: 'PC Gamer Amon, Ryzen 7 7700X, RTX 4060 Ti 8GB, 16GB, SSD M.2 1TB + SSD M.2 480GB',
+    code: '99999',
+    price: 11479.00,
+    discount: 5,
+    description: 'PC-GAMER',
+    images: [
+      'https://assets.onecompiler.app/42wu6ajnx/4392yzfbn/pc_6_img_1.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/4392yzfbn/pc_6_img_2.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/4392yzfbn/pc_6_img_3.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/4392yzfbn/pc_6_img_4.jpeg',
+    ],
+    detailedDescription:`
+      <div class="boxDescription_content">
+        <p><strong>Projetado para garantir o melhor desempenho em seus Jogos!</strong></p>
+  
+        <p>Os Computadores Brazil Pc, equipados com processadores Intel Core i5, têm desempenho surpreendente. Execute múltiplas tarefas ao mesmo tempo com fluidez.</p>
+        
+        <p>Compatível com os mais diversos jogos atuais, execute em alto desempenho sem lags ou travamentos, com armazenamento M.2, ligue e abra seus programas em segundos. Transfira seus dados facilmente entre dispositivos através da porta USB. Acompanha manual de instrução.</p>
+        
+        <p><strong>Especificações do produto abaixo:</strong></p>
+        
+        <p><strong>- Processador:</strong></p>
+        <p>Intel Core i5-11400 11ª geração</p>
+        
+        <p><strong>- Memória:</strong></p>
+        <p>Capacidade 16GB DDR4</p>
+        
+        <p><strong>- Armazenamento:</strong></p>
+        <p>Tipo: M.2, Capacidade: 512GB</p>
+        
+        <p><strong>- Placa de Vídeo:</strong></p>
+        <p>GEFORCE RTX3060M Golden Memory</p>
+        
+        <p><strong>- Fonte:</strong></p>
+        <p>500W Bivolt automática 80 Plus Bronze</p>
+        
+        <p><strong>- Tensão:</strong></p>
+        <p>110V/230V</p>
+        
+        <p><strong>- Gabinete:</strong></p>
+        <p>Tipo: Torre ATX com lateral em vidro temperado</p>
+      </div>
+    `
+  },
+  {
+    id: 7,
+    name: 'PC Gamer Éter, i5-13400F, RTX 4060 Ti 8GB, 16GB, SSD M.2 480GB',
+    code: '99999',
+    price: 9597.00,
+    discount: 5,
+    description: 'PC-GAMER',
+    images: [
+      'https://assets.onecompiler.app/42wu6ajnx/4392yzfbn/pc_7_img_1.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/4392yzfbn/pc_7_img_2.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/4392yzfbn/pc_7_img_3.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/4392yzfbn/pc_7_img_4.jpeg',
+    ],
+    detailedDescription:`
+      <div class="boxDescription_content">
+        <p><strong>Projetado para garantir o melhor desempenho em seus Jogos!</strong></p>
+  
+        <p>Os Computadores Brazil Pc, equipados com processadores Intel Core i5, têm desempenho surpreendente. Execute múltiplas tarefas ao mesmo tempo com fluidez.</p>
+        
+        <p>Compatível com os mais diversos jogos atuais, execute em alto desempenho sem lags ou travamentos, com armazenamento M.2, ligue e abra seus programas em segundos. Transfira seus dados facilmente entre dispositivos através da porta USB. Acompanha manual de instrução.</p>
+        
+        <p><strong>Especificações do produto abaixo:</strong></p>
+        
+        <p><strong>- Processador:</strong></p>
+        <p>Intel Core i5-11400 11ª geração</p>
+        
+        <p><strong>- Memória:</strong></p>
+        <p>Capacidade 16GB DDR4</p>
+        
+        <p><strong>- Armazenamento:</strong></p>
+        <p>Tipo: M.2, Capacidade: 512GB</p>
+        
+        <p><strong>- Placa de Vídeo:</strong></p>
+        <p>GEFORCE RTX3060M Golden Memory</p>
+        
+        <p><strong>- Fonte:</strong></p>
+        <p>500W Bivolt automática 80 Plus Bronze</p>
+        
+        <p><strong>- Tensão:</strong></p>
+        <p>110V/230V</p>
+        
+        <p><strong>- Gabinete:</strong></p>
+        <p>Tipo: Torre ATX com lateral em vidro temperado</p>
+      </div>
+    `
+  },
+  {
+    id: 8,
+    name: 'PC Gamer Fuzhu IV, i7-12700F, RTX 4060 Ti 8GB, 32GB, SSD M.2 1TB',
+    code: '99999',
+    price: 12000.00,
+    discount: 5,
+    description: 'PC-GAMER',
+    images: [
+      'https://assets.onecompiler.app/42wu6ajnx/4392yzfbn/pc_8_img_1.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/4392yzfbn/pc_8_img_2.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/4392yzfbn/pc_8_img_3.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/4392yzfbn/pc_8_img_4.jpeg',
+    ],
+    detailedDescription:`
+      <div class="boxDescription_content">
+        <p><strong>Projetado para garantir o melhor desempenho em seus Jogos!</strong></p>
+  
+        <p>Os Computadores Brazil Pc, equipados com processadores Intel Core i5, têm desempenho surpreendente. Execute múltiplas tarefas ao mesmo tempo com fluidez.</p>
+        
+        <p>Compatível com os mais diversos jogos atuais, execute em alto desempenho sem lags ou travamentos, com armazenamento M.2, ligue e abra seus programas em segundos. Transfira seus dados facilmente entre dispositivos através da porta USB. Acompanha manual de instrução.</p>
+        
+        <p><strong>Especificações do produto abaixo:</strong></p>
+        
+        <p><strong>- Processador:</strong></p>
+        <p>Intel Core i5-11400 11ª geração</p>
+        
+        <p><strong>- Memória:</strong></p>
+        <p>Capacidade 16GB DDR4</p>
+        
+        <p><strong>- Armazenamento:</strong></p>
+        <p>Tipo: M.2, Capacidade: 512GB</p>
+        
+        <p><strong>- Placa de Vídeo:</strong></p>
+        <p>GEFORCE RTX3060M Golden Memory</p>
+        
+        <p><strong>- Fonte:</strong></p>
+        <p>500W Bivolt automática 80 Plus Bronze</p>
+        
+        <p><strong>- Tensão:</strong></p>
+        <p>110V/230V</p>
+        
+        <p><strong>- Gabinete:</strong></p>
+        <p>Tipo: Torre ATX com lateral em vidro temperado</p>
+      </div>
+    `
+  },
+];
+
+
+// Função para formatar o preço
+function formatPrice(price) {
+  // Passo 1: Garantir que o número tenha 2 casas decimais
+  let formattedPrice = price.toFixed(2); 
+
+  // Passo 2: Substituir o ponto por vírgula
+  formattedPrice = formattedPrice.replace('.', ',');
+
+  // Passo 3: Adicionar o ponto como separador de milhar
+  formattedPrice = formattedPrice.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+
+  return formattedPrice;
+}
+
+
+// Função para buscar um produto pelo ID
+function getProductById(id) {
+  return products.find(product => product.id === id);
+}
