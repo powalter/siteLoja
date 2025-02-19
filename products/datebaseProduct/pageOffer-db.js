942dfe6eb2cad474abaaf1dda2360fb1e84f062b
Walter Miranda <101981943+powalter@users.noreply.github.com>
2025-02-19 17:54:13 -0300

    Add files via upload

---
+++ products/datebaseProduct/pageOffer-db.js
@@ -0,0 +1,415 @@
+const products = [
+  {
+    id: 1,
+    name: 'Gabinete Cooler Master HAF 700 EVO, ARGB, Full-Tower, Lateral de Vidro, Com 5 Fans, Cinza, H700E-IGNN-S00',
+    code: '99999',
+    price: 5889.00,
+    discount: 15,
+    description: 'PC-GAMER',
+    link: 'pageOffer.html?id=1',
+    image_1: 'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_1_img_1.jpeg',
+    image_2: 'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_1_img_2.jpeg',
+    images: [
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_1_img_1.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_1_img_2.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_1_img_3.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_1_img_4.jpeg'
+    ],
+    detailedDescription:`
+      <div class="boxDescription_content">
+        <p><strong>Projetado para garantir o melhor desempenho em seus Jogos!</strong></p>
+  
+        <p>Os Computadores Brazil Pc, equipados com processadores Intel Core i5, têm desempenho surpreendente. Execute múltiplas tarefas ao mesmo tempo com fluidez.</p>
+        
+        <p>Compatível com os mais diversos jogos atuais, execute em alto desempenho sem lags ou travamentos, com armazenamento M.2, ligue e abra seus programas em segundos. Transfira seus dados facilmente entre dispositivos através da porta USB. Acompanha manual de instrução.</p>
+        
+        <p><strong>Especificações do produto abaixo:</strong></p>
+        
+        <p><strong>- Processador:</strong></p>
+        <p>Intel Core i5-11400 11ª geração</p>
+        
+        <p><strong>- Memória:</strong></p>
+        <p>Capacidade 16GB DDR4</p>
+        
+        <p><strong>- Armazenamento:</strong></p>
+        <p>Tipo: M.2, Capacidade: 512GB</p>
+        
+        <p><strong>- Placa de Vídeo:</strong></p>
+        <p>GEFORCE RTX3060M Golden Memory</p>
+        
+        <p><strong>- Fonte:</strong></p>
+        <p>500W Bivolt automática 80 Plus Bronze</p>
+        
+        <p><strong>- Tensão:</strong></p>
+        <p>110V/230V</p>
+        
+        <p><strong>- Gabinete:</strong></p>
+        <p>Tipo: Torre ATX com lateral em vidro temperado</p>
+      </div>
+    `
+  },
+  {
+    id: 2,
+    name: 'Gabinete Gamer MSI MEG Maestro 700L PZ, Mid-Tower, Lateral de Vidro, Preto, 306-7G25L21-W57',
+    code: '99999',
+    price: 3992.00,
+    discount: 15,
+    description: 'PC-GAMER',
+    link: 'pageOffer.html?id=2',
+    image_1: 'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_2_img_1.jpeg',
+    image_2: 'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_2_img_2.jpeg',
+    images: [
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_2_img_1.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_2_img_2.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_2_img_3.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_2_img_4.jpeg'
+    ],
+    detailedDescription:`
+      <div class="boxDescription_content">
+        <p><strong>Projetado para garantir o melhor desempenho em seus Jogos!</strong></p>
+  
+        <p>Os Computadores Brazil Pc, equipados com processadores Intel Core i5, têm desempenho surpreendente. Execute múltiplas tarefas ao mesmo tempo com fluidez.</p>
+        
+        <p>Compatível com os mais diversos jogos atuais, execute em alto desempenho sem lags ou travamentos, com armazenamento M.2, ligue e abra seus programas em segundos. Transfira seus dados facilmente entre dispositivos através da porta USB. Acompanha manual de instrução.</p>
+        
+        <p><strong>Especificações do produto abaixo:</strong></p>
+        
+        <p><strong>- Processador:</strong></p>
+        <p>Intel Core i5-11400 11ª geração</p>
+        
+        <p><strong>- Memória:</strong></p>
+        <p>Capacidade 16GB DDR4</p>
+        
+        <p><strong>- Armazenamento:</strong></p>
+        <p>Tipo: M.2, Capacidade: 512GB</p>
+        
+        <p><strong>- Placa de Vídeo:</strong></p>
+        <p>GEFORCE RTX3060M Golden Memory</p>
+        
+        <p><strong>- Fonte:</strong></p>
+        <p>500W Bivolt automática 80 Plus Bronze</p>
+        
+        <p><strong>- Tensão:</strong></p>
+        <p>110V/230V</p>
+        
+        <p><strong>- Gabinete:</strong></p>
+        <p>Tipo: Torre ATX com lateral em vidro temperado</p>
+      </div>
+    `
+  },
+  {
+    id: 3,
+    name: 'Gabinete Gamer Hyte 60, Mid-Tower, Lateral de Vidro, Preto e Vermelho, CS-HYTE-Y60-BR',
+    code: '99999',
+    price: 2359.00,
+    discount: 15,
+    description: 'PC-GAMER',
+    link: 'pageOffer.html?id=3',
+    image_1: 'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_3_img_1.jpeg',
+    image_2: 'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_3_img_2.jpeg',
+    images: [
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_3_img_1.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_3_img_2.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_3_img_3.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_3_img_4.jpeg'
+    ],
+    detailedDescription:`
+      <div class="boxDescription_content">
+        <p><strong>Projetado para garantir o melhor desempenho em seus Jogos!</strong></p>
+  
+        <p>Os Computadores Brazil Pc, equipados com processadores Intel Core i5, têm desempenho surpreendente. Execute múltiplas tarefas ao mesmo tempo com fluidez.</p>
+        
+        <p>Compatível com os mais diversos jogos atuais, execute em alto desempenho sem lags ou travamentos, com armazenamento M.2, ligue e abra seus programas em segundos. Transfira seus dados facilmente entre dispositivos através da porta USB. Acompanha manual de instrução.</p>
+        
+        <p><strong>Especificações do produto abaixo:</strong></p>
+        
+        <p><strong>- Processador:</strong></p>
+        <p>Intel Core i5-11400 11ª geração</p>
+        
+        <p><strong>- Memória:</strong></p>
+        <p>Capacidade 16GB DDR4</p>
+        
+        <p><strong>- Armazenamento:</strong></p>
+        <p>Tipo: M.2, Capacidade: 512GB</p>
+        
+        <p><strong>- Placa de Vídeo:</strong></p>
+        <p>GEFORCE RTX3060M Golden Memory</p>
+        
+        <p><strong>- Fonte:</strong></p>
+        <p>500W Bivolt automática 80 Plus Bronze</p>
+        
+        <p><strong>- Tensão:</strong></p>
+        <p>110V/230V</p>
+        
+        <p><strong>- Gabinete:</strong></p>
+        <p>Tipo: Torre ATX com lateral em vidro temperado</p>
+      </div>
+    `
+  },
+  {
+    id: 4,
+    name: 'Gabinete Gamer Thermaltake CTE C750 Air, Preto, Full Tower, Vidro Temperado, E-ATX, Sem Fonte, Com 3 Fans, CA-1X6-00F1WN-00',
+    code: '99999',
+    price: 1976.35,
+    discount: 15,
+    description: 'PC-GAMER',
+    link: 'pageOffer.html?id=4',
+    image_1: 'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_4_img_1.jpeg',
+    image_2: 'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_4_img_2.jpeg',
+    images: [
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_4_img_1.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_4_img_2.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_4_img_3.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_4_img_4.jpeg'
+    ],
+    detailedDescription:`
+      <div class="boxDescription_content">
+        <p><strong>Projetado para garantir o melhor desempenho em seus Jogos!</strong></p>
+  
+        <p>Os Computadores Brazil Pc, equipados com processadores Intel Core i5, têm desempenho surpreendente. Execute múltiplas tarefas ao mesmo tempo com fluidez.</p>
+        
+        <p>Compatível com os mais diversos jogos atuais, execute em alto desempenho sem lags ou travamentos, com armazenamento M.2, ligue e abra seus programas em segundos. Transfira seus dados facilmente entre dispositivos através da porta USB. Acompanha manual de instrução.</p>
+        
+        <p><strong>Especificações do produto abaixo:</strong></p>
+        
+        <p><strong>- Processador:</strong></p>
+        <p>Intel Core i5-11400 11ª geração</p>
+        
+        <p><strong>- Memória:</strong></p>
+        <p>Capacidade 16GB DDR4</p>
+        
+        <p><strong>- Armazenamento:</strong></p>
+        <p>Tipo: M.2, Capacidade: 512GB</p>
+        
+        <p><strong>- Placa de Vídeo:</strong></p>
+        <p>GEFORCE RTX3060M Golden Memory</p>
+        
+        <p><strong>- Fonte:</strong></p>
+        <p>500W Bivolt automática 80 Plus Bronze</p>
+        
+        <p><strong>- Tensão:</strong></p>
+        <p>110V/230V</p>
+        
+        <p><strong>- Gabinete:</strong></p>
+        <p>Tipo: Torre ATX com lateral em vidro temperado</p>
+      </div>
+    `
+  },
+  {
+    id: 5,
+    name: 'Gabinete Gamer Cooler Master, Cosmos C700M RGB, Full Tower, Vidro Temperado, Black, Sem Fonte, Com 4 Fans, MCC-C700M-MG5N-S00',
+    code: '99999',
+    price: 3329.00,
+    discount: 15,
+    description: 'PC-GAMER',
+    link: 'pageOffer.html?id=5',
+    image_1: 'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_5_img_1.jpeg',
+    image_2: 'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_5_img_2.jpeg',
+    images: [
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_5_img_1.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_5_img_2.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_5_img_3.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_5_img_4.jpeg'
+    ],
+    detailedDescription:`
+      <div class="boxDescription_content">
+        <p><strong>Projetado para garantir o melhor desempenho em seus Jogos!</strong></p>
+  
+        <p>Os Computadores Brazil Pc, equipados com processadores Intel Core i5, têm desempenho surpreendente. Execute múltiplas tarefas ao mesmo tempo com fluidez.</p>
+        
+        <p>Compatível com os mais diversos jogos atuais, execute em alto desempenho sem lags ou travamentos, com armazenamento M.2, ligue e abra seus programas em segundos. Transfira seus dados facilmente entre dispositivos através da porta USB. Acompanha manual de instrução.</p>
+        
+        <p><strong>Especificações do produto abaixo:</strong></p>
+        
+        <p><strong>- Processador:</strong></p>
+        <p>Intel Core i5-11400 11ª geração</p>
+        
+        <p><strong>- Memória:</strong></p>
+        <p>Capacidade 16GB DDR4</p>
+        
+        <p><strong>- Armazenamento:</strong></p>
+        <p>Tipo: M.2, Capacidade: 512GB</p>
+        
+        <p><strong>- Placa de Vídeo:</strong></p>
+        <p>GEFORCE RTX3060M Golden Memory</p>
+        
+        <p><strong>- Fonte:</strong></p>
+        <p>500W Bivolt automática 80 Plus Bronze</p>
+        
+        <p><strong>- Tensão:</strong></p>
+        <p>110V/230V</p>
+        
+        <p><strong>- Gabinete:</strong></p>
+        <p>Tipo: Torre ATX com lateral em vidro temperado</p>
+      </div>
+    `
+  },
+  {
+    id: 6,
+    name: 'Gabinete Gamer Thermaltake Core P6 TG, Turquoise, Mid Tower, Vidro Temperado, ATX, Sem Fonte, Sem Fan, CA-1V2-00MBWN-00',
+    code: '99999',
+    price: 2235.18,
+    discount: 15,
+    description: 'PC-GAMER',
+    link: 'pageOffer.html?id=6',
+    image_1: 'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_6_img_1.jpeg',
+    image_2: 'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_6_img_2.jpeg',
+    images: [
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_6_img_1.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_6_img_2.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_6_img_3.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_6_img_4.jpeg'
+    ],
+    detailedDescription:`
+      <div class="boxDescription_content">
+        <p><strong>Projetado para garantir o melhor desempenho em seus Jogos!</strong></p>
+  
+        <p>Os Computadores Brazil Pc, equipados com processadores Intel Core i5, têm desempenho surpreendente. Execute múltiplas tarefas ao mesmo tempo com fluidez.</p>
+        
+        <p>Compatível com os mais diversos jogos atuais, execute em alto desempenho sem lags ou travamentos, com armazenamento M.2, ligue e abra seus programas em segundos. Transfira seus dados facilmente entre dispositivos através da porta USB. Acompanha manual de instrução.</p>
+        
+        <p><strong>Especificações do produto abaixo:</strong></p>
+        
+        <p><strong>- Processador:</strong></p>
+        <p>Intel Core i5-11400 11ª geração</p>
+        
+        <p><strong>- Memória:</strong></p>
+        <p>Capacidade 16GB DDR4</p>
+        
+        <p><strong>- Armazenamento:</strong></p>
+        <p>Tipo: M.2, Capacidade: 512GB</p>
+        
+        <p><strong>- Placa de Vídeo:</strong></p>
+        <p>GEFORCE RTX3060M Golden Memory</p>
+        
+        <p><strong>- Fonte:</strong></p>
+        <p>500W Bivolt automática 80 Plus Bronze</p>
+        
+        <p><strong>- Tensão:</strong></p>
+        <p>110V/230V</p>
+        
+        <p><strong>- Gabinete:</strong></p>
+        <p>Tipo: Torre ATX com lateral em vidro temperado</p>
+      </div>
+    `
+  },
+  {
+    id: 7,
+    name: 'Gabinete Gamer Thermaltake The Tower 600, Mid Tower, Vidro Temperado, ATX, Sem Fonte, Com 2 Fans, Preto, CA-1Z1-00M1WN-00',
+    code: '99999',
+    price: 2058.80,
+    discount: 15,
+    description: 'PC-GAMER',
+    link: 'pageOffer.html?id=7',
+    image_1: 'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_7_img_1.jpeg',
+    image_2: 'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_7_img_2.jpeg',
+    images: [
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_7_img_1.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_7_img_2.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_7_img_3.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_7_img_4.jpeg'
+    ],
+    detailedDescription:`
+      <div class="boxDescription_content">
+        <p><strong>Projetado para garantir o melhor desempenho em seus Jogos!</strong></p>
+  
+        <p>Os Computadores Brazil Pc, equipados com processadores Intel Core i5, têm desempenho surpreendente. Execute múltiplas tarefas ao mesmo tempo com fluidez.</p>
+        
+        <p>Compatível com os mais diversos jogos atuais, execute em alto desempenho sem lags ou travamentos, com armazenamento M.2, ligue e abra seus programas em segundos. Transfira seus dados facilmente entre dispositivos através da porta USB. Acompanha manual de instrução.</p>
+        
+        <p><strong>Especificações do produto abaixo:</strong></p>
+        
+        <p><strong>- Processador:</strong></p>
+        <p>Intel Core i5-11400 11ª geração</p>
+        
+        <p><strong>- Memória:</strong></p>
+        <p>Capacidade 16GB DDR4</p>
+        
+        <p><strong>- Armazenamento:</strong></p>
+        <p>Tipo: M.2, Capacidade: 512GB</p>
+        
+        <p><strong>- Placa de Vídeo:</strong></p>
+        <p>GEFORCE RTX3060M Golden Memory</p>
+        
+        <p><strong>- Fonte:</strong></p>
+        <p>500W Bivolt automática 80 Plus Bronze</p>
+        
+        <p><strong>- Tensão:</strong></p>
+        <p>110V/230V</p>
+        
+        <p><strong>- Gabinete:</strong></p>
+        <p>Tipo: Torre ATX com lateral em vidro temperado</p>
+      </div>
+    `
+  },
+  {
+    id: 8,
+    name: 'Gabinete Gamer Fractal Design North Chalk White, Mid Tower, Mesh, ATX, Sem Fonte, Com 2 Fans, FD-C-NOR1C-03',
+    code: '99999',
+    price: 5889.00,
+    discount: 15,
+    description: 'PC-GAMER',
+    link: 'pageOffer.html?id=8',
+    image_1: 'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_8_img_1.jpeg',
+    image_2: 'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_8_img_2.jpeg',
+    images: [
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_8_img_1.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_8_img_2.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_8_img_3.jpeg',
+      'https://assets.onecompiler.app/42wu6ajnx/439c49cn7/gab_8_img_4.jpeg'
+    ],
+    detailedDescription:`
+      <div class="boxDescription_content">
+        <p><strong>Projetado para garantir o melhor desempenho em seus Jogos!</strong></p>
+  
+        <p>Os Computadores Brazil Pc, equipados com processadores Intel Core i5, têm desempenho surpreendente. Execute múltiplas tarefas ao mesmo tempo com fluidez.</p>
+        
+        <p>Compatível com os mais diversos jogos atuais, execute em alto desempenho sem lags ou travamentos, com armazenamento M.2, ligue e abra seus programas em segundos. Transfira seus dados facilmente entre dispositivos através da porta USB. Acompanha manual de instrução.</p>
+        
+        <p><strong>Especificações do produto abaixo:</strong></p>
+        
+        <p><strong>- Processador:</strong></p>
+        <p>Intel Core i5-11400 11ª geração</p>
+        
+        <p><strong>- Memória:</strong></p>
+        <p>Capacidade 16GB DDR4</p>
+        
+        <p><strong>- Armazenamento:</strong></p>
+        <p>Tipo: M.2, Capacidade: 512GB</p>
+        
+        <p><strong>- Placa de Vídeo:</strong></p>
+        <p>GEFORCE RTX3060M Golden Memory</p>
+        
+        <p><strong>- Fonte:</strong></p>
+        <p>500W Bivolt automática 80 Plus Bronze</p>
+        
+        <p><strong>- Tensão:</strong></p>
+        <p>110V/230V</p>
+        
+        <p><strong>- Gabinete:</strong></p>
+        <p>Tipo: Torre ATX com lateral em vidro temperado</p>
+      </div>
+    `
+  },
+];
+
+
+// Função para formatar o preço
+function formatPrice(price) {
+  // Passo 1: Garantir que o número tenha 2 casas decimais
+  let formattedPrice = price.toFixed(2); 
+
+  // Passo 2: Substituir o ponto por vírgula
+  formattedPrice = formattedPrice.replace('.', ',');
+
+  // Passo 3: Adicionar o ponto como separador de milhar
+  formattedPrice = formattedPrice.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+
+  return formattedPrice;
+}
+
+
+// Função para buscar um produto pelo ID
+function getProductById(id) {
+  return products.find(product => product.id === id);
+}
