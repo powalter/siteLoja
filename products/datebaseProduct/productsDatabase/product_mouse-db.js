b577ec41e67c5b1ec40ff336a2b53e4bd7437d59
Walter Miranda <101981943+powalter@users.noreply.github.com>
2025-11-21 17:18:51 -0300

    Update product images and maintain structure

--- products/datebaseProduct/productsDatabase/product_mouse-db.js
+++ products/datebaseProduct/productsDatabase/product_mouse-db.js
@@ -31,13 +31,13 @@ const products = [
     category: 'MOUSE',
     description: 'PC-GAMER',
     link: '../../../pagesProduct/pageProduct/products/page_product_mouse.html?id=2',
-    image_1: '../../../../imagens/produtos/mouse/mouse_1_img_1.jpeg',
-    image_2: '../../../../imagens/produtos/mouse/mouse_1_img_2.jpeg',
+    image_1: '../../../../imagens/produtos/mouse/mouse_2_img_1.jpeg',
+    image_2: '../../../../imagens/produtos/mouse/mouse_2_img_2.jpeg',
     images: [
-      '../../../../imagens/produtos/mouse/mouse_1_img_1.jpeg',
-      '../../../../imagens/produtos/mouse/mouse_1_img_2.jpeg',
-      '../../../../imagens/produtos/mouse/mouse_1_img_3.jpeg',
-      '../../../../imagens/produtos/mouse/mouse_1_img_4.jpeg'
+      '../../../../imagens/produtos/mouse/mouse_2_img_1.jpeg',
+      '../../../../imagens/produtos/mouse/mouse_2_img_2.jpeg',
+      '../../../../imagens/produtos/mouse/mouse_2_img_3.jpeg',
+      '../../../../imagens/produtos/mouse/mouse_2_img_4.jpeg'
     ],
     detailedDescription: 'Carregando . . .'
   },
@@ -52,13 +52,13 @@ const products = [
     category: 'MOUSE',
     description: 'PC-GAMER',
     link: '../../../pagesProduct/pageProduct/products/page_product_mouse.html?id=3',
-    image_1: '../../../../imagens/produtos/mouse/mouse_1_img_1.jpeg',
-    image_2: '../../../../imagens/produtos/mouse/mouse_1_img_2.jpeg',
+    image_1: '../../../../imagens/produtos/mouse/mouse_3_img_1.jpeg',
+    image_2: '../../../../imagens/produtos/mouse/mouse_3_img_2.jpeg',
     images: [
-      '../../../../imagens/produtos/mouse/mouse_1_img_1.jpeg',
-      '../../../../imagens/produtos/mouse/mouse_1_img_2.jpeg',
-      '../../../../imagens/produtos/mouse/mouse_1_img_3.jpeg',
-      '../../../../imagens/produtos/mouse/mouse_1_img_4.jpeg'
+      '../../../../imagens/produtos/mouse/mouse_3_img_1.jpeg',
+      '../../../../imagens/produtos/mouse/mouse_3_img_2.jpeg',
+      '../../../../imagens/produtos/mouse/mouse_3_img_3.jpeg',
+      '../../../../imagens/produtos/mouse/mouse_3_img_4.jpeg'
     ],
     detailedDescription: 'Carregando . . .'
   },
@@ -73,13 +73,13 @@ const products = [
     category: 'MOUSE',
     description: 'PC-GAMER',
     link: '../../../pagesProduct/pageProduct/products/page_product_mouse.html?id=4',
-    image_1: '../../../../imagens/produtos/mouse/mouse_1_img_1.jpeg',
-    image_2: '../../../../imagens/produtos/mouse/mouse_1_img_2.jpeg',
+    image_1: '../../../../imagens/produtos/mouse/mouse_4_img_1.jpeg',
+    image_2: '../../../../imagens/produtos/mouse/mouse_4_img_2.jpeg',
     images: [
-      '../../../../imagens/produtos/mouse/mouse_1_img_1.jpeg',
-      '../../../../imagens/produtos/mouse/mouse_1_img_2.jpeg',
-      '../../../../imagens/produtos/mouse/mouse_1_img_3.jpeg',
-      '../../../../imagens/produtos/mouse/mouse_1_img_4.jpeg'
+      '../../../../imagens/produtos/mouse/mouse_4_img_1.jpeg',
+      '../../../../imagens/produtos/mouse/mouse_4_img_2.jpeg',
+      '../../../../imagens/produtos/mouse/mouse_4_img_3.jpeg',
+      '../../../../imagens/produtos/mouse/mouse_4_img_4.jpeg'
     ],
     detailedDescription: 'Carregando . . .'
   },
@@ -105,4 +105,5 @@ function formatPrice(price) {
 function getProductById(id) {
   return products.find(product => product.id === id);
 }
+
 
